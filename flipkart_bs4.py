eaa7fa0f8de1fad25f8f67c8fd1049c92e5f0405
sakthisethu <sakthiraghav71@gmail.com>
2016-11-29 15:04:08 +0530

    task

---
+++ flipkart_bs4.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+url="https://www.flipkart.com/search?q=mobi&otracker=start&as-show=on&as=off"
+req=requests.get(url)
+req.content
+
+soup=BeautifulSoup(req.content)
+for links in soup.find_all("link"):
+	print links.get("href")
+
+for links in soup.find_all("meta"):
+	print links.get("name")
+
+for links in soup.find_all("meta"):
+	print links.get("content")
